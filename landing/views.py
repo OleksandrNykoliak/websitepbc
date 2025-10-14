62291a50cc244dc51401685e4ee0921b1ff19f95
OleksandrNykoliak <nykolyak.o@gmail.com>
2025-10-14 22:14:22 +0300

    upd
    Signed-off-by: OleksandrNykoliak <nykolyak.o@gmail.com>

--- landing/views.py
+++ landing/views.py
@@ -4,29 +4,4 @@ def homepage_view(request):
     return render(request, "base.html")
 
 def s(request):
-    return render(request, "s.html")
-
-# def projects_view(request):
-#     return render(request, "projects.html")
-
-# def about_view(request):
-#     return render(request, "about.html")
-
-
-# def about2_view(request):
-#     return render(request, "about2.html")
-
-# def services_view(request):
-#     return render(request, "services.html")
-
-
-# def team_view(request):
-#     return render(request, "team.html")
-
-
-# def customer_view(request):
-#     return render(request, "customers.html")
-
-
-# def contact_view(request):
-#     return render(request, "contact.html")
+    return render(request, "services_hover.html")
